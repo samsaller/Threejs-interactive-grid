a4198dedf7cb38fa3d13b330ffb6c1912cbfc7d5
Sam <85741949+samsaller@users.noreply.github.com>
2024-12-15 00:25:13 +0500

    Fix

--- src/main.js
+++ src/main.js
@@ -111,3 +111,9 @@ window.addEventListener("resize", (e) => {
     camera.updateProjectionMatrix();
     renderer.setSize(sizes.width, sizes.height);
 });
+
+// canvas.addEventListener("dblclick", (e) => {
+//     document.fullscreenElement
+//         ? document.exitFullscreen()
+//         : canvas.requestFullscreen();
+// });
